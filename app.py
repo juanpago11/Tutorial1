5f9785fb79dfa5919c61410ee0359778674a1866
juanpago11 <juanpago11@gmail.com>
2026-02-26 10:29:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,18 @@ st.write("El bhuo puede gobernar el mundo")
 image = Image.open('buhoo.jpg')
 st.image(image, caption='EL BUHO')
 
-Ttexto = st.text_input('escribe algo', 'este es mi texto')
+texto = st.text_input('escribe algo', 'este es mi texto')
 st.write('EL texto es la prote', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+coll, col2 = st.columns(2)
+
+with coll:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimpodales mejoran la UX")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+      st.write('Correcto')
+               
+    
