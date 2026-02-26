de0c4e558774079aba47f45407586620152f7554
juanpago11 <juanpago11@gmail.com>
2026-02-26 10:09:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from PIL import image
 st.title("Hola de nuevo mundo jaja")
 st.header("En este espacio comienza el nuevo orden mundial")
 st.write("Facilmente el bhuo puede gobernar el mundo")
-image = image.open('buho.jpg')
+image = image.open('buhoo.jpg')
 st.image(image, caption='EL BUHO')
 
