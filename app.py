149cdf1049403aa70e892e1ac9887444f5d71741
juanpago11 <juanpago11@gmail.com>
2026-02-26 10:20:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ st.write("El bhuo puede gobernar el mundo")
 image = Image.open('buhoo.jpg')
 st.image(image, caption='EL BUHO')
 
+Ttexto = st.text_input('escribe algo', 'este es mi texto')
+st.write('EL texto es la prote', texto)
