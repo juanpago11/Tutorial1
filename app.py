f66547aec0cecdfefb6d895d25499f2700d717b1
juanpago11 <juanpago11@gmail.com>
2026-02-26 10:11:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from PIL import image
+from PIL import Image
 
 st.title("Hola de nuevo mundo jaja")
 st.header("En este espacio comienza el nuevo orden mundial")
 st.write("Facilmente el bhuo puede gobernar el mundo")
-image = image.open('buhoo.jpg')
+image = Image.open('buhoo.jpg')
 st.image(image, caption='EL BUHO')
 
